8f0dd9c800a4b255589f3a55c678ec9b91fa5917
Selvin Raúl Chuquiej Andrade <schuquiej.a@gmail.com>
2025-08-19 07:46:07 -0600

    Creacion del modelo

---
+++ Practica/model/registroLlamada.js
@@ -0,0 +1,9 @@
+export default class CallRecord {
+    constructor(id_operador, nombre_operador, no_estrellas, id_cliente, nombre_cliente) {
+        this.id_operador = id_operador;
+        this.nombre_operador = nombre_operador;
+        this.no_estrellas = no_estrellas;
+        this.id_cliente = id_cliente;
+        this.nombre_cliente = nombre_cliente;
+    }
+}
